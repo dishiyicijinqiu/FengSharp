5c0fe170696d8b3375abf13b2e03c7bea2597175
dishiyicijinqiu <458141536@qq.com>
2015-08-09 11:47:55 +0800

    sdf

---
+++ FengSharp.WinForm.Dev/Form_Exit.designer.cs
@@ -0,0 +1,121 @@
+﻿namespace FengSharp.WinForm.Dev
+{
+    partial class Form_Exit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Exit));
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.pictureEdit1 = new DevExpress.XtraEditors.PictureEdit();
+            this.btnReTry = new DevExpress.XtraEditors.SimpleButton();
+            this.btnNo = new DevExpress.XtraEditors.SimpleButton();
+            this.btnYes = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(113, 36);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(108, 13);
+            this.labelControl1.TabIndex = 14;
+            this.labelControl1.Text = "确实要退出系统吗？";
+            // 
+            // pictureEdit1
+            // 
+            this.pictureEdit1.EditValue = ((object)(resources.GetObject("pictureEdit1.EditValue")));
+            this.pictureEdit1.Location = new System.Drawing.Point(65, 25);
+            this.pictureEdit1.Name = "pictureEdit1";
+            this.pictureEdit1.Properties.AllowFocused = false;
+            this.pictureEdit1.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.pictureEdit1.Properties.Appearance.Options.UseBackColor = true;
+            this.pictureEdit1.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.pictureEdit1.Properties.ReadOnly = true;
+            this.pictureEdit1.Properties.ShowMenu = false;
+            this.pictureEdit1.Size = new System.Drawing.Size(42, 34);
+            this.pictureEdit1.TabIndex = 13;
+            // 
+            // btnReTry
+            // 
+            this.btnReTry.Location = new System.Drawing.Point(222, 76);
+            this.btnReTry.Name = "btnReTry";
+            this.btnReTry.Size = new System.Drawing.Size(81, 26);
+            this.btnReTry.TabIndex = 12;
+            this.btnReTry.Text = "重新登录(&R)";
+            this.btnReTry.Click += new System.EventHandler(this.btnReTry_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnNo.Location = new System.Drawing.Point(126, 76);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(81, 26);
+            this.btnNo.TabIndex = 11;
+            this.btnNo.Text = "否(&N)";
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // btnYes
+            // 
+            this.btnYes.Location = new System.Drawing.Point(27, 76);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(81, 26);
+            this.btnYes.TabIndex = 10;
+            this.btnYes.Text = "是(&Y)";
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // Form_Exit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnNo;
+            this.ClientSize = new System.Drawing.Size(330, 135);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.pictureEdit1);
+            this.Controls.Add(this.btnReTry);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnYes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form_Exit";
+            this.Text = "提示";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Exit_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.PictureEdit pictureEdit1;
+        private DevExpress.XtraEditors.SimpleButton btnReTry;
+        private DevExpress.XtraEditors.SimpleButton btnNo;
+        private DevExpress.XtraEditors.SimpleButton btnYes;
+    }
+}
---
+++ FengSharp.WinForm.Dev/TextEdit_TimeStyle.cs
@@ -0,0 +1,74 @@
+﻿using DevExpress.XtraEditors;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace FengSharp.WinForm.Dev
+{
+
+    [ToolboxItem(true)]
+    [Description("是否应用时间样式。")]
+    [ProvideProperty("TimeStyleIsApply", typeof(TextEdit))]
+    public class TextEdit_TimeStyle : Component, IExtenderProvider
+    {
+        private Dictionary<TextEdit, bool> TimeStyleList = null;
+        public TextEdit_TimeStyle()
+        {
+            TimeStyleList = new Dictionary<TextEdit, bool>();
+        }
+
+        public TextEdit_TimeStyle(IContainer container)
+        {
+            container.Add(this);
+            TimeStyleList = new Dictionary<TextEdit, bool>();
+        }
+
+        [Category("扩展")]
+        [Description("是否应用时间样式")]
+        public bool GetTimeStyleIsApply(TextEdit textEdit)
+        {
+            if (TimeStyleList.ContainsKey(textEdit))
+            {
+                return (bool)TimeStyleList[textEdit];
+            }
+            return false;
+        }
+        public void SetTimeStyleIsApply(TextEdit textEdit, bool isApply)
+        {
+            if (!TimeStyleList.ContainsKey(textEdit))
+            {
+                TimeStyleList.Add(textEdit, isApply);
+            }
+            else
+            {
+                TimeStyleList[textEdit] = isApply;
+            }
+            if (isApply)
+            {
+                textEdit.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime;
+                textEdit.Properties.EditFormat.FormatType = DevExpress.Utils.FormatType.DateTime;
+                textEdit.Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.DateTime;
+                if (string.IsNullOrWhiteSpace(TimeStyleFormatString))
+                {
+                    textEdit.Properties.DisplayFormat.FormatString = "yyyy-MM-dd HH:mm:ss";
+                    textEdit.Properties.EditFormat.FormatString = "yyyy-MM-dd HH:mm:ss";
+                    textEdit.Properties.Mask.EditMask = "yyyy-MM-dd HH:mm:ss";
+                }
+                else
+                {
+                    textEdit.Properties.DisplayFormat.FormatString = TimeStyleFormatString;
+                    textEdit.Properties.EditFormat.FormatString = TimeStyleFormatString;
+                    textEdit.Properties.Mask.EditMask = TimeStyleFormatString;
+                }
+            }
+        }
+        public bool CanExtend(object extendee)
+        {
+            return (extendee is TextEdit);
+        }
+        [Category("扩展")]
+        [Description("时间样式的格式")]
+        [DefaultValue("yyyy-MM-dd HH:mm:ss")]
+        public string TimeStyleFormatString { get; set; }
+
+    }
+}
